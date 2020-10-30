e6db2892792ee481d57d49e3bbbe8563f3661e61
tgwow <thiago.2809@outlook.com.br>
2020-10-30 16:16:18 -0300

    feat: layout and core functionalities done

--- src/js/script.js
+++ src/js/script.js
@@ -1,114 +1,156 @@
-import { elt, convertToArr, generateRandomNumbers, compare } from './utils.js'
+import { elt, generateRandomNumbers, compare } from './utils.js'
+import DOM from './DOM.js';
 
-((window, document) => {
+((window, document, $) => {
     'use strict';
 
     const MAX = 25;
+    const $gameBoard = $('section[data-js="game-board"]').get();
+    const $betList = $('ul[data-js="bet-list"]').get();
 
-    const $loto = document.querySelector('.loto__numbers');
-    const $spanCounter = document.querySelector('span');
-    const $checkResult = document.querySelector('button[data-js="button-result"]');
-    const $newGame = document.querySelector('button[data-js="button-new"]');
-    const $lotoResults = document.querySelector('.loto__result');
-    const $lotoConcurso = document.querySelector('.loto__concurso');
-    // preciso utlizar esse método, pois, neste ponto do código, os botoes não foram criados ainda
-    const $lotoNumbers = document.getElementsByClassName('button-number');
+    const $completeGame = $('button[data-js="button-complete"]');
+    const $saveGame = $('button[data-js="button-save"]');
+    const $clearGame = $('button[data-js="button-clear"]');
 
-    let numbersArr = [];
+    const $hitsList = $('div[data-js="hits-list"]').get();
+    const $championBtn = $('button[data-js="btn-champion"]')
+    const $championInput = $('input[data-js="input-champion"]').get();
+
+    let $gameNumbers
     let selectedNumbers = [];
-    let correctNumbers = [];
     let games = [];
     let hits = [];
+    let id = 1;
 
     function init() {
         initNumbers();
-        generateRandomNumbers(correctNumbers, 15, MAX);
-        numbersArr = convertToArr($lotoNumbers);
+        $gameNumbers = getGameNumbers();
         registerEvents();
-        console.log(numbersArr)
     }
-    function initNumbers() {
+    function initNumbers($gameNumbers) {
         for(let i = 0; i < MAX; i++) {
-            if (i % 5 === 0) {
-                const row = elt('div', { class: 'loto__row'})
-                $loto.appendChild(row);
+            if (i % 7 === 0) {
+                const row = elt('div', { class: 'game__bet__row'});
+                $gameBoard.appendChild(row);
             }
-            const number = elt('button', { class: 'button-number','data-js': 'button-number', value: i + 1}, String(i + 1));
-            $loto.lastElementChild.appendChild(number);
+            let btnNumberAttributes = { class: 'btn-number','data-js': 'button-number', value: i + 1};
+            const btnNumber = elt('button', btnNumberAttributes , String(i + 1));
+            $gameBoard.lastElementChild.appendChild(btnNumber);
         }
     }
+
+    function getGameNumbers() {  return $('.btn-number') }
 
     function registerEvents() {
-        numbersArr.forEach((button) => {
+        $gameNumbers.forEach((button) => {
             button.addEventListener('click', handleButtonClick);
         });
-        $checkResult.addEventListener('click', handleResult);
-        $newGame.addEventListener('click', handleNewGame);
+        $clearGame.on('click', handleClearGame);
+        $saveGame.on('click', handleSaveGame);
+        $completeGame.on('click', handleCompleteGame);
+        $championBtn.on('click', handleChampionResult)
 
     }
-
-    function handleNewGame() {
-        if (selectedNumbers.length < 15) {
-            alert('Voce deve selecionar 15 números!');
-            return;
-        }
-        games.push(selectedNumbers);
-        clearButtonsNumbers();
+    function handleClearGame() {
         selectedNumbers = [];
+        clearNumbers();
     }
-
-    function clearButtonsNumbers() {
-        $spanCounter.textContent = 'Números selecionados: 0';
-        numbersArr.forEach((item) => {
+    function clearNumbers() {
+        $gameNumbers.forEach((item) => {
             item.classList.remove('active');
         })
     }
-    function handleResult() {
-        if (selectedNumbers.length < 15) {
+
+    function handleSaveGame() {
+        if (selectedNumbers.length < 5) {
             alert('Voce deve selecionar 15 números!');
             return;
         }
-        games.push(selectedNumbers);
-        checkHitsPerGame();
-        showHitsResult();
-        showCorrectNumbers();
-        clearButtonsNumbers();
-        resetArrays();
-        generateRandomNumbers(correctNumbers, 15, MAX);
-    }
-    function resetArrays() {
-        selectedNumbers = [];
-        games = [];
-        hits = [];
-        correctNumbers = [];
-    }
 
-    function showCorrectNumbers() {
-        const p = elt('p', null, 'Resultado do concurso');
-        const span = elt('p', {'class': 'loto__concurso__result'}, correctNumbers.sort(compare).join(', '));
-        $lotoConcurso.textContent = '';
-        $lotoConcurso.appendChild(p);
-        $lotoConcurso.appendChild(span);
+        games.push({numbers:selectedNumbers, id});
+        addGameToList(selectedNumbers.sort(compare), games.length, id);
+        id++
+        handleClearGame();
     }
+    function addGameToList(numbers, index, id) {
+        const bet = createBetItem(numbers, index, id);
+        $betList.appendChild(bet);
+    }
+    function createBetItem(numbers, index, id) {
+        const betGameNumber = elt('div', {class: 'bet__list__item__game'}, String('#'+index));
+        const betNumbers = elt('div', {class: 'bet__list__item__numbers'}, numbers.toString());
+        const btnClose = elt('span', {class: 'bet__close', 'data-js': id}, 'x');
+        btnClose.addEventListener('click', handleRemoveBet);
+        const bet = elt('li', {class: 'bet__list__item'}, betGameNumber, betNumbers, btnClose);
 
-    function checkHitsPerGame() {
+        return bet;
+    }
+    function updateBetList() {
+        $betList.innerHTML='';
         games.forEach((game, index) => {
-            hits.push(game.filter((number) => {
-                return correctNumbers.indexOf(+number) !== -1;
-            }));
+            const bet = createBetItem(game.numbers,index+1, game.id)
+            $betList.appendChild(bet);
+        })
+    }
+
+    function handleRemoveBet(button) {
+        const id = this.getAttribute('data-js');
+        removeBetFromGamesArray(id);
+        removeBetFromGamesList(button);
+        updateBetList();
+    }
+
+    function removeBetFromGamesArray(id) {
+        games = games.filter(game => game.id !== +id);
+    }
+    function removeBetFromGamesList(button) {
+        button.target.parentNode.remove();
+    }
+
+
+    function handleCompleteGame() {
+        const amount = selectedNumbers.length
+        generateRandomNumbers(selectedNumbers, 15 - amount, 25);
+        fillSelectedNumbersOnBoard();
+    }
+
+    function fillSelectedNumbersOnBoard(){
+        selectedNumbers.forEach(number => {
+            const item = $gameNumbers.find(gameNumber => {
+                return +gameNumber.value === number;
+            })
+            item.classList.add('active');
         });
     }
 
-    function showHitsResult() {
-        $lotoResults.textContent = '';
+    function handleChampionResult() {
+        const splitedValue = $championInput.value.split(',').map(item => +item);
+        getHitsPerGame(splitedValue);
+        showHitsList();
+        console.log('hits> ', hits);
+        console.log('game >', games);
+        console.log('selected >', selectedNumbers);
+        resetArrays();
+    }
+
+    function resetArrays() {
+        selectedNumbers = [];
+        hits = [];
+    }
+
+    function getHitsPerGame(splitedValue) {
+        games.forEach((game, index) => {
+            hits.push(game.numbers.filter((number) => {
+                return splitedValue.indexOf(number) !== -1;
+            }));
+        });
+    }
+    // 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+    function showHitsList() {
+        $hitsList.innerHTML = '';
         hits.forEach((hit, index, arr) => {
-            const result = elt('span', null,`O jogo ${index + 1}, teve ${hit.length} acertos` );
-            result.appendChild(elt('span', {'class': 'loto__result__numbers'}, `[${hit.sort(compare)}]`));
-            $lotoResults.appendChild(elt(
-                'div',
-                {'class': 'loto__result__row'},
-                result
-            ));
+            const hitsParagraph = elt('p', {class: 'bet__result__game'},`O jogo #${index + 1}, teve ${hit.length} acertos` );
+            $hitsList.appendChild(hitsParagraph);
         })
     }
 
@@ -123,14 +165,12 @@ import { elt, convertToArr, generateRandomNumbers, compare } from './utils.js'
         } else {
             // n chegou no limite
             if (selectedNumbers.length < 15)
-                selectedNumbers.push(this.value);
+                selectedNumbers.push(+this.value);
             else{
                 this.classList.remove('active');
                 alert('Quantidade máxima: 15 números');
             }
         }
-        $spanCounter.textContent = `Números selecionados: ${selectedNumbers.length}`;
     }
-
     init();
-})(window, document);
+})(window, document, DOM);
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,4 @@
-export function elt(tag, attributes, args) {
+export function elt(tag, attributes, ...args) {
     const elt = document.createElement(tag);
     // se tiver atributos para serem adicionados ao elemento
     if (attributes)
@@ -11,15 +11,16 @@ export function elt(tag, attributes, args) {
     // terceiro parametro para adicionar um valor de texto ao nó
     // let child = args;
     if (args) {
-        let child = args;
-        if (typeof args === 'string')
-            child = document.createTextNode(args);
-        elt.appendChild(child);
+        for(let i = 0; i < args.length; i++) {
+            let child = args[i];
+            if (typeof child === 'string')
+                child = document.createTextNode(args[i]);
+            elt.appendChild(child);
+        }
     }
     return elt;
 }
-export function convertToArr(arrayLike) {
-    // retorna um array com todos valores de um array like
+export function convertArrayLikeToArray(arrayLike) {
     return Array.prototype.map.call(arrayLike, (item) => {
         return item;
     });
@@ -29,20 +30,16 @@ export function convertToArr(arrayLike) {
 export function generateRandomNumbers(numbers, qtd, range) {
     for (let i = 1; i <= qtd ; i++) {
         let number = randomNumber(range);
-        // enquanto já existir o numero no array,
-        // gera outro numero e testa até encontrar um numero unico
         while (numbers.indexOf(number) !== -1)
             number = randomNumber(range);
 
         numbers.push(number);
     }
 }
-// gera um random entre 1 e range
 function randomNumber(range) {
     return Math.floor(Math.random() * range + 1);
 }
 
-// funcao para ordernacao crescente no sort()
 export function compare(a, b) {
     return a - b;
 }
